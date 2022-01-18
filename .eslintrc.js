7b95339a24dfd845112331e5b1588a597f6556f5
HinanJiHyun <wlgusdldi15@naver.com>
2022-01-18 17:08:00 +0900

    cslint add

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:vue/essential",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "vue/html-closing-bracket-newline": ["error", {
+            "singleline": "never",
+            "multiline": "never"
+        }],
+        "vue/html-self-closing": ["error", {
+        "html": {
+            "void": "always",
+            "normal": "always",
+            "component": "always"
+        },
+        "svg": "always",
+        "math": "always"
+        }]
+    }
+}
